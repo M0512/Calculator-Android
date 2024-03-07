043abc595bf2c5b11a7739734c2c365745a1b207
unknown <mufaron2002@gmail.com>
2024-03-07 16:15:53 +0200

    initial commit

---
+++ app/src/main/java/com/example/constraintlayout/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.constraintlayout.databinding.ActivityMainBinding
+
+
+private lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnApply.setOnClickListener {
+            val firstName= binding.etfirstName.text.toString()
+            val secondName=binding.etlastName.text.toString()
+            val birthDate= binding.etbirthDate.text.toString()
+            val country= binding.etcountry.text.toString()
+binding.tedisplay.setText("my name is "+firstName+" "+secondName+" I was born on "+birthDate+" i live in "+country)
+
+        }
+
+
+
+
+
+
+
+    }
+}
